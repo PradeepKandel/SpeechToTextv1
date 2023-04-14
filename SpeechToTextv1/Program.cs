40d6042354542442d2b98425397c62bb6d14e22b
Pradeep Kandel <pradeep_kandel@hotmail.com>
2023-04-14 11:12:28 +0800

    Add project files.

---
+++ SpeechToTextv1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+
+namespace SpeechToTextv1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await RecognizeSpeech();
+                Console.WriteLine("Finished.");
+            
+        }
+        private static async Task RecognizeSpeech()
+        {
+            var configuration = SpeechConfig.FromSubscription("d59ccab6eb834624b468b81bc3fabef9","southeastasia");
+            using (var recog = new SpeechRecognizer(configuration))
+            {
+                Console.WriteLine("Speak something now");
+                var result = await recog.RecognizeOnceAsync();
+                if (result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    Console.WriteLine(result.Text);
+
+                }
+            }
+        }
+    }
+}
